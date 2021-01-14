3ed2334e671060cc82e4ec298f2cb8b9504296d4
Sidx-sys <sid.iiith@outlook.com>
2021-01-14 17:50:22 +0530

    Updated routing logic

--- api/routes/login.js
+++ api/routes/login.js
@@ -3,119 +3,135 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
 
 const JobApplicant = require("../../models/Job_Applicants");
 const JobRecruiter = require("../../models/Job_Recruiters");
 
-// @route POST api/login/app
-// @desc Login an existing job applicant
+// @route POST api/login
+// @desc Login an existing user
 // @access public
-router.post("/app", (req, res) => {
+router.post("/", (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password)
-        return res.status(400).json({ msg: "All fields are not filled" })
+        return res.status(400).json({ msg: "All fields are not filled" });
 
     JobApplicant.findOne({ email }).then((applicant) => {
         if (!applicant) {
+            JobRecruiter.findOne({ email }).then((recruiter) => {
+                bcrypt.compare(password, recruiter.password).then((isMatch) => {
+                    if (!isMatch)
+                        return res
+                            .status(400)
+                            .json({ msg: "Invalid Password" });
+
+                    jwt.sign(
+                        { id: recruiter._id },
+                        config.get("jwtSecret"),
+                        {
+                            expiresIn: 7600,
+                        },
+                        (err, token) => {
+                            if (err) throw err;
+                            res.json({
+                                token,
+                                recruiter: {
+                                    id: recruiter._id,
+                                    name: recruiter.name,
+                                    email: recruiter.email,
+                                    contact: recruiter.contact,
+                                    bio: recruiter.bio,
+                                    rating: recruiter.rating,
+                                },
+                            });
+                        }
+                    );
+                });
+            });
+        } else if (applicant) {
+            bcrypt.compare(password, applicant.password).then((isMatch) => {
+                if (!isMatch)
+                    return res.status(400).json({ msg: "Invalid Password" });
+
+                jwt.sign(
+                    { id: applicant._id },
+                    config.get("jwtSecret"),
+                    {
+                        expiresIn: 7600,
+                    },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({
+                            token,
+                            applicant: {
+                                id: applicant._id,
+                                name: applicant.name,
+                                email: applicant.email,
+                                academics: applicant.academics,
+                                skills: applicant.skills,
+                                rating: applicant.rating,
+                            },
+                        });
+                    }
+                );
+            });
+        } else {
             return res.status(400).json({ msg: "Email not found" });
         }
-
-        bcrypt.compare(password, applicant.password).then((isMatch) => {
-            if (!isMatch)
-                return res
-                    .status(400)
-                    .json({ msg: "Invalid Password" });
-
-            jwt.sign(
-                { id: applicant._id },
-                config.get("jwtSecret"),
-                {
-                    expiresIn: 7600,
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({
-                        token,
-                        applicant: {
-                            id: applicant._id,
-                            name: applicant.name,
-                            email: applicant.email,
-                            academics: applicant.academics,
-                            skills: applicant.skills,
-                            rating: applicant.rating
-                        },
-                    });
-                }
-            );
-        });
-    })
-})
+    });
+});
 
 // @route api/login/app
 // @desc GET job applicant data
 // @access Private
 router.get("/app", auth, (req, res) => {
-    JobApplicant
-        .findById(req.user.id)
+    JobApplicant.findById(req.user.id)
         .select("-password")
-        .then(user => res.json(user));
+        .then((user) => res.json(user));
 });
 
-// @route POST api/login/rec
-// @desc Login an existing recruiter
+// @route api/login/app/tokenIsValid
+// @desc checks if a given token is valid or not
 // @access public
-router.post("/rec", (req, res) => {
-    const { email, password } = req.body;
 
-    if (!email || !password)
-        return res.status(400).json({ msg: "All fields are not filled" })
+router.post("/app/tokenIsValid", (req, res) => {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
 
-    JobRecruiter.findOne({ email }).then((recruiter) => {
-        if (!recruiter) {
-            return res.status(400).json({ msg: "Email not found" });
-        }
+    const verified = jwt.verify(token, config.get("jwtSecret"));
+    if (!verified) return res.json(false);
 
-        bcrypt.compare(password, recruiter.password).then((isMatch) => {
-            if (!isMatch)
-                return res
-                    .status(400)
-                    .json({ msg: "Invalid Password" });
+    const user = JobApplicant.findById(verified.id);
+    if (!user) return res.json(false);
 
-            jwt.sign(
-                { id: recruiter._id },
-                config.get("jwtSecret"),
-                {
-                    expiresIn: 7600,
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({
-                        token,
-                        recruiter: {
-                            id: recruiter._id,
-                            name: recruiter.name,
-                            email: recruiter.email,
-                            contact: recruiter.contact,
-                            bio: recruiter.bio,
-                            rating: recruiter.rating
-                        },
-                    });
-                }
-            );
-        });
-    })
-})
+    return res.json(true);
+});
 
 // @route api/login/rec
 // @desc GET recruiter data
 // @access Private
 router.get("/rec", auth, (req, res) => {
-    JobRecruiter
-        .findById(req.recruiter.id)
+    JobRecruiter.findById(req.recruiter.id)
         .select("-password")
-        .then(user => res.json(user));
+        .then((user) => res.json(user));
+});
+
+// @route api/login/rec/tokenIsValid
+// @desc checks if a given token is valid or not
+// @access public
+
+router.post("/rec/tokenIsValid", (req, res) => {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+
+    const verified = jwt.verify(token, config.get("jwtSecret"));
+    if (!verified) return res.json(false);
+
+    const user = JobRecruiter.findById(verified.id);
+    if (!user) return res.json(false);
+
+    return res.json(true);
 });
 
 module.exports = router;
--- api/routes/register.js
+++ api/routes/register.js
@@ -14,53 +14,67 @@ router.post("/app", (req, res) => {
     const { name, email, password, password2 } = req.body;
 
     if (!email || !name || !password || !password2)
-        return res.status(400).json({ msg: "All fields are not filled" })
+        return res.status(400).json({ msg: "All fields are not filled" });
     if (password.length < 5)
-        return res.status(400).json({ msg: "Password length has to be greater than 5 characters" })
-    if (password !== password2)
-        return res.status(400).json({ msg: "Confirmed password doesn't match" })
-
-    JobApplicant.findOne({ email }).then((applicant) => {
-        if (applicant) {
-            return res.status(400).json({ msg: "User already exists" });
-        }
-        const newApplicant = new JobApplicant({
-            name,
-            email,
-            password,
+        return res.status(400).json({
+            msg: "Password length has to be greater than 5 characters",
         });
+    if (password !== password2)
+        return res
+            .status(400)
+            .json({ msg: "Confirmed password doesn't match" });
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newApplicant.password, salt, (err, hash) => {
-                if (err) throw err;
-                newApplicant.password = hash;
-                newApplicant.save().then((applicant) => {
-                    jwt.sign(
-                        { id: applicant._id },
-                        config.get("jwtSecret"),
-                        {
-                            expiresIn: 7600,
-                        },
-                        (err, token) => {
-                            if (err) throw err;
-                            res.json({
-                                token,
-                                applicant: {
-                                    id: applicant._id,
-                                    name: applicant.name,
-                                    email: applicant.email,
-                                    academics: applicant.academics,
-                                    skills: applicant.skills,
-                                    rating: applicant.rating
-                                },
-                            });
-                        }
-                    );
+    JobRecruiter.findOne({ email }).then((recruiter) => {
+        if (recruiter) {
+            return res
+                .status(400)
+                .json({ msg: "User already exists as Recruiter" });
+        }
+
+        JobApplicant.findOne({ email }).then((applicant) => {
+            if (applicant) {
+                return res
+                    .status(400)
+                    .json({ msg: "Applicant already exists" });
+            }
+            const newApplicant = new JobApplicant({
+                name,
+                email,
+                password,
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newApplicant.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newApplicant.password = hash;
+                    newApplicant.save().then((applicant) => {
+                        jwt.sign(
+                            { id: applicant._id },
+                            config.get("jwtSecret"),
+                            {
+                                expiresIn: 7600,
+                            },
+                            (err, token) => {
+                                if (err) throw err;
+                                res.json({
+                                    token,
+                                    applicant: {
+                                        id: applicant._id,
+                                        name: applicant.name,
+                                        email: applicant.email,
+                                        academics: applicant.academics,
+                                        skills: applicant.skills,
+                                        rating: applicant.rating,
+                                    },
+                                });
+                            }
+                        );
+                    });
                 });
             });
         });
-    })
-})
+    });
+});
 
 // @route api/register/rec
 // @desc Register a new Recruiter
@@ -69,55 +83,66 @@ router.post("/rec", (req, res) => {
     const { name, email, password, password2, contact } = req.body;
 
     if (!email || !name || !password || !password2 || contact)
-        return res.status(400).json({ msg: "All fields are not filled" })
+        return res.status(400).json({ msg: "All fields are not filled" });
     if (password.length < 5)
-        return res.status(400).json({ msg: "Password length has to be greater than 5 characters" })
-    if (password !== password2)
-        return res.status(400).json({ msg: "Confirmed password doesn't match" })
-    if (contact.length !== 10)
-        return res.status(400).json({ msg: "Invalid contact number" })
-
-    JobRecruiter.findOne({ email }).then((recruiter) => {
-        if (recruiter) {
-            return res.status(400).json({ msg: "User already present" });
-        }
-        const newRecruiter = new JobRecruiter({
-            name,
-            email,
-            password,
-            contact
+        return res.status(400).json({
+            msg: "Password length has to be greater than 5 characters",
         });
+    if (password !== password2)
+        return res
+            .status(400)
+            .json({ msg: "Confirmed password doesn't match" });
+    if (contact.length !== 10)
+        return res.status(400).json({ msg: "Invalid contact number" });
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newRecruiter.password, salt, (err, hash) => {
-                if (err) throw err;
-                newRecruiter.password = hash;
-                newRecruiter.save().then((recruiter) => {
-                    jwt.sign(
-                        { id: recruiter._id },
-                        config.get("jwtSecret"),
-                        {
-                            expiresIn: 7600,
-                        },
-                        (err, token) => {
-                            if (err) throw err;
-                            res.json({
-                                token,
-                                recruiter: {
-                                    id: recruiter._id,
-                                    name: recruiter.name,
-                                    email: recruiter.email,
-                                    contact: recruiter.contact,
-                                    bio: recruiter.bio,
-                                    rating: recruiter.rating
-                                },
-                            });
-                        }
-                    );
+    JobApplicant.findOne({ email }).then((applicant) => {
+        if (applicant) {
+            return res
+                .status(400)
+                .json({ msg: "User already exists as Applicant" });
+        }
+        JobRecruiter.findOne({ email }).then((recruiter) => {
+            if (recruiter) {
+                return res.status(400).json({ msg: "User already present" });
+            }
+            const newRecruiter = new JobRecruiter({
+                name,
+                email,
+                password,
+                contact,
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newRecruiter.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newRecruiter.password = hash;
+                    newRecruiter.save().then((recruiter) => {
+                        jwt.sign(
+                            { id: recruiter._id },
+                            config.get("jwtSecret"),
+                            {
+                                expiresIn: 7600,
+                            },
+                            (err, token) => {
+                                if (err) throw err;
+                                res.json({
+                                    token,
+                                    recruiter: {
+                                        id: recruiter._id,
+                                        name: recruiter.name,
+                                        email: recruiter.email,
+                                        contact: recruiter.contact,
+                                        bio: recruiter.bio,
+                                        rating: recruiter.rating,
+                                    },
+                                });
+                            }
+                        );
+                    });
                 });
             });
         });
-    })
-})
+    });
+});
 
 module.exports = router;
